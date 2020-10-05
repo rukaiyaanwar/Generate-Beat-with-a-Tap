4dadc3d3e1a8ddf5d5054d785bd5a4a70305a9c7
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2020-10-05 18:26:33 +0530

    generated music with color balls

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#7160d3",
+        "#d358aa"
+    ];
+
+    pads.forEach((pad,index) => {
+        pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+            createBubbles(index);
+        });
+    });
+
+    //Create function that makes bubble
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor =colors[index];
+        bubble.style.animation = `jump 1s ease`;
+        bubble.addEventListener("animationend", function() {
+            visual.removeChild(this);
+        });
+    };
+});
